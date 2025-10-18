f5bfbf0aac7b03d4da42105f3e74ce3e78790199
s27523 <s27523@pjwstk.edu.pl>
2025-10-19 00:18:37 +0200

    task 3: Katalog danych (catalog.yml) i parametry (parameters.yml)
    Add catalog.yml config, add parameters.yml config, adjust nodes.py and pipeline.py, add to environment.yml - kedro-datasets and kedro-datasets[tracking] dependencies

--- src/asi_group_15_01/pipelines/data_science/pipeline.py
+++ src/asi_group_15_01/pipelines/data_science/pipeline.py
@@ -6,13 +6,12 @@ generated using Kedro 1.0.0
 from kedro.pipeline import Node, Pipeline
 from .nodes import load_raw, basic_clean, train_test_split, train_baseline, evaluate
 
-
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
             Node(
                 func=load_raw,
-                inputs=None,
+                inputs="params:data.raw_csv",
                 outputs="raw_data",
                 name="load",
             ),
@@ -24,7 +23,7 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             Node(
                 func=train_test_split,
-                inputs="cleaned_data",
+                inputs=["cleaned_data", "params:split"],
                 outputs=[
                     "X_train",
                     "X_test",
@@ -35,7 +34,7 @@ def create_pipeline(**kwargs) -> Pipeline:
             ),
             Node(
                 func=train_baseline,
-                inputs=["X_train", "y_train"],
+                inputs=["X_train", "y_train", "params:model"],
                 outputs="baseline_model",
                 name="train_baseline",
             ),
